b0cc2b0e9f1375c4fa38261c4bc0b34f9f91ab79
Anna Yoon <annaeyoon9@gmail.com>
2025-09-20 23:00:51 -0700

    Added resume download button

--- src/App.jsx
+++ src/App.jsx
@@ -6,11 +6,20 @@ function App() {
 
   return (
     <>
-      <h1>Hi, I'm Anna</h1>
-      <div className="card">
+      <div classname="intro">
+        <h1 className="intro-text">Hi, I'm Anna</h1>
+      </div>
+      
+      {/* <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
+      </div> */}
+
+      <div classname="resume-download">
+        <a href='/Anna Yoon - Resume.pdf' download>
+          <button class='buttonDownload'>Download Resume</button>
+        </a>
       </div>
     </>
   )
