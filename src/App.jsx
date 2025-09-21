c43d867a98950a5b8ec12073a3138ef6ef2eee05
Anna Yoon <annaeyoon9@gmail.com>
2025-09-20 23:25:54 -0700

    Added about me

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,37 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [typedText, setTypedText] = useState("");
+  const fullText = "Hi, I'm Anna!";
+
+  useEffect(() => {
+    let currentIndex = 0;
+    const interval = setInterval(() => {
+      setTypedText(fullText.slice(0, currentIndex + 1));
+      currentIndex++;
+      if (currentIndex === fullText.length) {
+        clearInterval(interval);
+      }
+    }, 200);
+    return () => clearInterval(interval);
+  }, []);
 
   return (
     <>
-      <div classname="intro">
-        <h1 className="intro-text">Hi, I'm Anna</h1>
+      <div className="intro">
+        <h1 className="intro-text">{typedText}</h1>
       </div>
-      
-      {/* <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-      </div> */}
 
-      <div classname="resume-download">
+      <div className="resume-download">
         <a href='/Anna Yoon - Resume.pdf' download>
-          <button class='buttonDownload'>Download Resume</button>
+          <button className="buttonDownload">Download Resume/CV</button>
         </a>
+      </div>
+
+      <div className="about-me">
+        <h2>About Me</h2>
+        <p className="about-me-text">I'm a recent Computer Science graduate from University of California, Irvine. </p>
       </div>
     </>
   )
